ef4d5951e13ffa95af34fb32f9548c703a83c1f0
Rashad-Stack <tex.eng.rashed@gmail.com>
2022-11-05 03:00:58 +0600

    Google authentication added

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,35 @@
 import Image from "next/image";
+import { useContext } from "react";
+import { MediumContext } from "../context/MediumContext";
 import Logo from "../static/logo.png";
 import Button from "./Button";
 
 function Header() {
+  const { handleUserAuth, currentUser } = useContext(MediumContext);
+
   return (
     <header className="flex justify-center items-center gap-10 p-5 bg-yellow-500">
       <div className="container max-w-7xl mx-auto flex justify-between items-center">
         <div className="w-40 h-10 flex items-center flex-start cursor-pointer">
           <Image alt="Logo" src={Logo} className="w-full object-contain" />
         </div>
-        <nav className="flex cursor-pointer items-center space-x-5">
-          <div>Our Story</div>
-          <div>Membership</div>
-          <div>Sign in</div>
-          <Button>Get Started</Button>
-        </nav>
+        {currentUser ? (
+          <nav className="flex cursor-pointer items-center space-x-5">
+            <button className="highlight-none">Our Story</button>
+            <button className="highlight-none">Membership</button>
+            <Button className="highlight-none">Write</Button>
+            <Button className="highlight-none">Get Started</Button>
+          </nav>
+        ) : (
+          <nav className="flex cursor-pointer items-center space-x-5">
+            <button className="highlight-none">Our Story</button>
+            <button className="highlight-none">Membership</button>
+            <button className="highlight-none" onClick={handleUserAuth}>
+              Sign in
+            </button>
+            <Button className="highlight-none">Get Started</Button>
+          </nav>
+        )}
       </div>
     </header>
   );
--- context/MediumContext.js
+++ context/MediumContext.js
@@ -1,11 +1,13 @@
-import { collection, getDocs, setDoc, doc } from "firebase/firestore";
+import { signInWithPopup } from "firebase/auth";
+import { collection, doc, getDocs, setDoc } from "firebase/firestore";
 import { createContext, useEffect, useState } from "react";
-import { fireStore } from "../firebase";
+import { auth, fireStore, provider } from "../firebase";
 
 const MediumContext = createContext();
 const MediumContextProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
   const [posts, setPosts] = useState([]);
+  const [currentUser, setCurrentUser] = useState([]);
 
   useEffect(() => {
     const getUsers = async () => {
@@ -47,8 +49,28 @@ const MediumContextProvider = ({ children }) => {
     };
     getUsers();
   }, []);
+
+  const addUserToFirebase = async (user) => {
+    await setDoc(doc(fireStore, "users", user.email), {
+      email: user.email,
+      name: user.displayName,
+      imageUrl: user.photoURL,
+      followerCount: 0,
+    });
+  };
+
+  const handleUserAuth = async () => {
+    const userData = await signInWithPopup(auth, provider);
+    const user = userData.user;
+    setCurrentUser(user);
+    addUserToFirebase(user);
+    console.log(user, "🍌 👍");
+  };
+
   return (
-    <MediumContext.Provider value={{ posts, users }}>
+    <MediumContext.Provider
+      value={{ posts, users, handleUserAuth, currentUser }}
+    >
       {children}
     </MediumContext.Provider>
   );
